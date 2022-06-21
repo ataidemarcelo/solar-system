7ae968de73ef1d41a4f0eed01de4089da152a23b
Marcelo Ataíde <msa.trybe@gmail.com>
2022-06-21 15:01:10 -0300

    Requisito 01 - Cria um componente chamado Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
